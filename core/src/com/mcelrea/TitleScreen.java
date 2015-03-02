474d6b65844d044c8dc682edaed9451bfb69effb
mcelrea <mcelrea@damien-hs.edu>
2015-03-02 13:07:25 -0800

    Recommit Title Screen

--- core/src/com/mcelrea/TitleScreen.java
+++ core/src/com/mcelrea/TitleScreen.java
@@ -1,7 +1,52 @@
 package com.mcelrea;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
 /**
  * Created by Tech on 3/2/2015.
  */
-public class TitleScreen {
+public class TitleScreen implements Screen{
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,1,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            ((Game)Gdx.app.getApplicationListener()).setScreen(new GameplayScreen());
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
